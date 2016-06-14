34cc8a12968eff70e411b45679fac83a4cb24fa7
ianjonsen <ian.jonsen@mq.edu.au>
2016-06-14 20:09:27 +1000

    citations added

---
+++ inst/CITATION.R
@@ -0,0 +1,38 @@
+citHeader("To cite bsam in publications use:")
+
+citEntry(entry = "Article",
+         title        = "Robust state-space modeling of animal movement data",
+         author       = personList(as.person("Ian D. Jonsen"),
+                                   as.person("Joanna Mills Flemming"), 
+                                   as.person("Ransom A. Myers")),
+         journal      = "Ecology",
+         year         = "2005",
+         volume       = "86",
+         number       = "11",
+         pages        = "2874--2880",
+         url          = "http://www.jstatsoft.org/v40/i03/",
+         
+         textVersion  =
+           paste("Jonsen ID, Mills Flemming J, Myers RA (2005)",
+                 "Robust state-space modeling of animal movement data.",
+                 "Ecology 86:2874-2880")
+)
+
+citEntry(entry = "Article",
+         title        = "Joint estimation over multiple individuals improves behavioural 
+state inference from animal movement data",
+         author       = personList(as.person("Ian Jonsen")),
+         journal      = "Scientific Reports",
+         year         = "2016",
+         volume       = "6",
+         number       = "",
+         pages        = "20625",
+         url          = "http://www.jstatsoft.org/v40/i03/",
+         
+         textVersion  =
+           paste("Jonsen I (2016)",
+                 "Joint estimation over multiple individuals improves behavioural 
+state inference from animal movement data.",
+                 "Scientific Reports 6:20625")
+)
+
